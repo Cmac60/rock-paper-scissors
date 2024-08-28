b10e8cbff4f7699e8d01f31a1c4b3b94a4cbfd8f
zeo <165603081+Cmac60@users.noreply.github.com>
2024-08-27 22:52:38 -0500

    Create function playRound to announce round winner, increment winner score and show computer choice. Make player input case-insensitive for getHumanChoice()

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let humanScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
     let choice = Math.random();
     if (choice <= 0.33) {
@@ -14,16 +17,42 @@ function getComputerChoice() {
 
 function getHumanChoice() {
     let result = prompt("rock, paper or scissors?: ");
-    if (result === "rock") {
+    let cResult = result.toLowerCase();
+    if (cResult === "rock") {
         return "rock";
-    } else if (result === "paper") {
+    } else if (cResult === "paper") {
         return "paper";
-    } else if (result === "scissors") {
+    } else if (cResult === "scissors") {
         return "scissors";
     } else {
         return "please input valid choice"
     }
 }
-console.log(getHumanChoice());
 
-console.log(getComputerChoice());
+function playRound(humanChoice, computerChoice) {
+    humanChoice = getHumanChoice();
+    computerChoice = getComputerChoice();
+    if (humanChoice === "rock" && computerChoice === "scissors") {
+        console.log("You win! Rock beats scissors.");
+        ++humanScore;
+    } else if (humanChoice === "paper" && computerChoice === "rock") {
+        console.log("You win! Paper beats rock.");
+        ++humanScore;
+    } else if (humanChoice === "scissors" && computerChoice === "paper") {
+        console.log("You win! Scissors beats paper.");
+        ++humanScore;
+    } else if (humanChoice === "rock" && computerChoice === "paper") {
+        console.log("You lose! Paper beats rock.");
+        ++computerScore;
+    } else if (humanChoice === "paper" && computerChoice === "scissors") {
+        console.log("You lose! Scissors beats paper.");
+        ++computerScore;
+    } else if (humanChoice === "scissors" && computerChoice === "rock") {
+        console.log("You lose! Rock beats scissors.");
+        ++computerScore;
+    } else {
+        console.log("Draw!");
+    }
+    return computerChoice;
+}
+console.log(playRound());
